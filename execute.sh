3f16ff99c5cd07881886955ead00ceafaa55c014
Brian Broll <brian.broll@vanderbilt.edu>
2018-04-25 10:41:20 -0500

    only run 5 in parallel

--- execute.sh
+++ execute.sh
@@ -5,7 +5,7 @@ echo "MAX_INDEX $MAX_INDEX"
 INDICES=$(seq 0 1 $MAX_INDEX)
 rm download-has-failed.txt 2> /dev/null # This marks if a download has failed
 
-echo $INDICES | tr " " "\n" | parallel -j50 bash $CMD.sh {}
+echo $INDICES | tr " " "\n" | parallel -j5 bash $CMD.sh {}
 
 # Uncomment to download serially
 #for index in $INDICES; do
