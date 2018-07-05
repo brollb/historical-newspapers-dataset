33adaf5d8c454aa385516676d13ef11a38268ccc
Brian Broll <brian.broll@vanderbilt.edu>
2018-07-05 08:32:24 -0500

    Added some additional logging for debugging

--- dataset.py
+++ dataset.py
@@ -15,15 +15,21 @@ class DataLoader(object):
         for f in files:
             print(f)
             with tarfile.open(path.join(self.dirname, f), 'r:bz2') as t:
+                print('opened file', f)
                 for info in t:
                     if '.txt' in info.name:
+                        print('about to read in text from ' + info.name)
                         year = int(info.name.split('/')[1])
                         if self.start <= year and year <= self.end:
+                            print('extracting file')
                             tf = t.extractfile(info)
                             document = tf.read()
+                            print('read file')
                             for sentence in get_sentences(str(document)):
                                 # Get the sentences
                                 # This is pretty noisy data currently and could use
                                 # some preprocessing
-                                print(utils.simple_preprocess(sentence))
-                                yield utils.simple_preprocess(sentence)
+                                print('about to preprocess')
+                                preprocessed = utils.simple_preprocess(sentence)
+                                print(preprocessed)
+                                yield preprocessed
